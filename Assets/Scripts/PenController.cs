d4da79c767f2b23cde08c5be7ffe19f97771dc6a
matavock <fifermas@gmail.com>
2026-03-25 14:29:14 +0300

    Update PenController.cs

--- Assets/Scripts/PenController.cs
+++ Assets/Scripts/PenController.cs
@@ -1,8 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PenController : MonoBehaviour
 {
     public GameObject extraButtonsPanel;
+    public Key hotkey = Key.Tab;
+
+    void Update()
+    {
+        if (Keyboard.current != null && Keyboard.current[hotkey].wasPressedThisFrame)
+        {
+            OnPenClicked();
+        }
+    }
 
     public void OnPenClicked()
     {
